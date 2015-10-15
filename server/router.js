64352ec275aaed9c43f1fe248d89f91c020daf9d
naomiajacobs <naomiajacobs@gmail.com>
2015-10-15 12:09:08 -0700

    Insert pseudocode for server routes

---
+++ server/router.js
@@ -0,0 +1,27 @@
+// ''
+	//get: serve static files
+// '/users'
+	//post: add new user
+// '/users/:username'
+	//get: get username info
+	//put: update user info
+	//delete: delete user
+// '/households'
+	//post: add new household
+// '/households/:householdID'
+	//put: update household
+	//delete: delete household
+// '/listentries'
+	//post: add new list entry
+// '/listentries/:listentryID'
+	//put: update list entry
+	//delete: delete list entry
+// '/reconciliations'
+	//post: 
+	//put:
+	//delete:
+// '/reconciliations/:reconciliationID'
+	//get:
+	//post:
+	//put:
+	//delete:

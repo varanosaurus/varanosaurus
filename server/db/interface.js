95be1313952069cb15051f751a428c9a10b3bf64
camhux <camzig@gmail.com>
2015-10-16 10:51:19 -0700

    Clearer names for configuration modules

--- server/db/interface.js
+++ server/db/interface.js
@@ -3,10 +3,10 @@ var Sequelize = require('sequelize');
 var config = require('./postgres.config.js');
 var url = process.env.DATABASE_URL; //SET THIS UP PROPERLY SOON
 
-var listItem = require('./models/ListItem.js');
-var household = require('./models/Household.js');
-var reckoning = require('./models/Reckoning.js');
-var user = require('./models/User.js');
+var listItemConfig = require('./models/ListItem.js');
+var householdConfig = require('./models/Household.js');
+var reckoningConfig = require('./models/Reckoning.js');
+var userConfig = require('./models/User.js');
 
 var dbEnvironment = process.env.NODE_ENV;
 
@@ -16,16 +16,16 @@ var schema = 'knead';
 //yell at Naomi if she forgets to get back to this
 var db = new Sequelize(url, {ssl: true, {schema: schema}});
 
-var ListItem = db.define('listItem', listItem.attributes, listItem.options);
+var ListItem = db.define('listItem', listItemConfig.attributes, listItemConfig.options);
 ListItem.schema(schema);
 
-var Household = db.define('household', household.attributes, household.options);
+var Household = db.define('household', householdConfig.attributes, householdConfig.options);
 Household.schema(schema);
 
-var Reckoning = db.define('reckoning', reckoning.attributes, reckoning.options);
+var Reckoning = db.define('reckoning', reckoningConfig.attributes, reckoningConfig.options);
 Reckoning.schema(schema);
 
-var User = db.define('user', user.attributes, user.options);
+var User = db.define('user', userConfig.attributes, userConfig.options);
 User.schema(schema);
 
 ListItem.belongsTo(Household);

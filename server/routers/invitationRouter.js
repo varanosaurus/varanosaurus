787ba35bdb396155cc2685462bfb5ca49b52c49c
camhux <camzig@gmail.com>
2015-10-21 16:29:10 -0700

    Add update and delete routes for invitations

--- server/routers/invitationRouter.js
+++ server/routers/invitationRouter.js
@@ -33,14 +33,48 @@ router.get(function(request, response) {
   var userId = request.decoded.userId;
 
   db.Invitation.findAll({where: {toUserId: userId}})
+
     .then(function(invitations) {
       response.json(invitations);
     })
+
     .catch(function(error) {
       console.error(error);
       response.status(500).send(error);
     });
 
 });
+
+router.route('/:invitationId')
+
+  .put(function(request, response) {
+
+    var updates = request.body;
+
+    db.Invitation.update(updates, {where: {id: request.params.invitationId}})
+
+      .then(function() {
+        response.sendStatus(200);
+      })
+
+      .catch(function(error) {
+        response.status(500).send(error);
+      });
+
+  })
+
+  .delete(function(request, response) {
+
+    db.Invitation.destroy({where: {id: request.params.invitationId}})
+
+      .then(function() {
+        response.sendStatus(200);
+      })
+
+      .catch(function(error) {
+        response.status(500).send(error);
+      });
+
+  });
 
 module.exports = router;

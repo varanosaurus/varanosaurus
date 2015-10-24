d02e19e4300823a01b9aec7cf9baf24b22d0b344
camhux <camzig@gmail.com>
2015-10-24 12:20:26 -0700

    Begin refactor of components

---
+++ client-mobile/newOrg/Components/Entry/dumb/Signup.js
@@ -0,0 +1,90 @@
+'use strict';
+
+var React = require('react-native');
+
+var {
+  StyleSheet,
+  TouchableHighlight,
+  View,
+  Text,
+  TextInput,
+} = React;
+
+var Signup = React.createClass({
+
+  getInitialState() {
+    return ({
+       username: '',
+       password: '',
+       error: '',
+    });
+  },
+
+  render() {
+    return (
+          <View style={styles.container}>
+          <TextInput
+            keyboardType='default'
+            style={styles.input}
+            placeholder='username'
+            onChangeText={(username) => this.setState({username})}
+            value={this.state.username}
+          />
+          <TextInput
+            keyboardType='default'
+            style={styles.input}
+            placeholder='password'
+            secureTextEntry={true}
+            onChangeText={(password) => this.setState({password})}
+            value={this.state.password}
+          />
+          // <Text style={styles.errorHandling}>{this.props.error}</Text>
+          <TouchableHighlight style={styles.button} onPress={this.handleSubmit}>
+            <Text style={styles.btnText}>Sign up</Text>
+          </TouchableHighlight>
+
+          <Text>Already have an account?</Text>
+          <TouchableHighlight style={styles.button} onPress={this.props.gotoLogin}>
+            <Text style={styles.btnText}>Log in</Text>
+          </TouchableHighlight>
+        </View>);
+  },
+
+  handleSubmit() {
+    var data = {username: this.state.username, password: this.state.password};
+    this.props.submit(data);
+  },
+
+});
+
+module.exports = Signup;
+
+var styles = StyleSheet.create({
+  container: {
+    marginTop: 64,
+    flex: 1,
+    flexDirection: 'column',
+    backgroundColor: 'white',
+  },
+  input: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+  },
+  button: {
+    flex: 1,
+    paddingTop: 10,
+    paddingBottom: 10,
+    backgroundColor: 'black',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    position: 'absolute',
+  },
+  btnText: {
+    fontSize: 18,
+    color: 'white',
+  },
+  errorHandling: {
+    color: 'red',
+  },
+});

4730b4c21812133e13aa1af1c1f0883b54410fd6
camhux <camzig@gmail.com>
2015-10-28 14:36:51 -0700

    Abandon attemping to chain navigator state to store; get rendering again

--- client-mobile/newOrg/Components/NavRedux.js
+++ client-mobile/newOrg/Components/NavRedux.js
@@ -1,8 +1,6 @@
 'use strict';
 
 var React = require('react-native');
-// var NavBar = require('react-native-navbar');
-var {connect} = require('react-redux');
 
 // var Actions = require('../Actions/Actions');
 var Routes = require('../Services/Routes');
@@ -11,69 +9,50 @@ var {
   Navigator,
   TouchableHighlight,
   Text,
+  View,
 } = React;
 
 
 var NavRedux = React.createClass({
 
-  componentWillMount() {
-    var navBar = <Navigator.NavigationBar
-      title={this.props.routes[this.props.routes.length - 1].title}
-      routeMapper={this.routeMapper}
-    />;
-
-    this.navigator = <Navigator
-      initialRoute={this.props.initialRoute}
-      renderScene={this.renderScene}
-      configureScene={this.configureScene}
-      navigationBar={navBar}
-      ref='navigator'
-      />;
-  },
-
-  componentWillReceiveProps(nextProps) {
-    console.log('hello from componentWillReceiveProps');
-    var next = nextProps.routes;
-    console.log(nextProps);
-    console.dir(this);
-    var current = this.refs.navigator.getCurrentRoutes();
-    console.dir(current);
-    var i;
-
-    for (i = 0; i < Math.max(next.length, current.length); i++) {
-
-      if (next[i] && !current[i]) {
-        this.push(next[i]);
-        break;
-      }
-
-      if (!next[i] && current[i]) {
-        this.pop();
-        break;
-      }
-
-    }
-  },
+  // componentWillMount() {
+  //   this.navigator = <Navigator
+  //     initialRoute={Routes.hometab}
+  //     renderScene={this.renderScene}
+  //     configureScene={this.configureScene}
+  //     ref='navigator'
+  //     />;
+  // },
 
   render() {
-    console.log('rendering navbar');
-    return this.navigator;
+
+    return <Navigator
+      initialRoute={Routes.hometab}
+      renderScene={this.renderScene}
+      configureScene={this.configureScene}
+      ref='navigator'
+      />;
 
   },
 
   renderScene(route) {
+    var NavBar = Navigator.NavigationBar;
     var Component = route.component;
-    return (
-      <Component {...route.props} />
-    );
+
+    if (route.name === 'hometab') {
+      return <Component />;
+    } else {
+      return (
+        <View>
+          <NavBar title={route.title} routeMapper={this.routeMapper} />
+          <Component {...route.props} />
+        </View>
+      );
+    }
   },
 
   configureScene(route) {
     return route.sceneConfig || Navigator.SceneConfigs.FloatFromRight;
-  },
-
-  goBack() {
-    this.dispatch(/*TODO: Actions.popRoute()*/);
   },
 
   routeMapper: {
@@ -81,10 +60,14 @@ var NavRedux = React.createClass({
     LeftButton(route, navigator, index) {
       return (<TouchableHighlight style={{marginTop: 30}} onPress={() => {
         if (index > 0) {
-          this.goBack();
+          this.navigator.pop();
         }
       }}>
-      <Text>Back</Text>
+      {() => {
+        if (index > 0) {
+          return <Text>{this.navigator.getCurrentRoutes()[index - 1].title}</Text>;
+        }
+      }}
       </TouchableHighlight>
       );
     },
@@ -101,14 +84,4 @@ var NavRedux = React.createClass({
 
 });
 
-function select(state) {
-  var routes = state.routes.map((routeName) => Routes[routeName]());
-  var initialRoute = Routes[state.initialRoute]();
-
-  return {
-    routes,
-    initialRoute,
-  };
-}
-
-module.exports = connect(select)(NavRedux);
+module.exports = NavRedux;

a5f5dfa2223677ade1a4080a53a666d87d4630fe
camhux <camzig@gmail.com>
2015-10-28 14:36:51 -0700

    Abandon attemping to chain navigator state to store; get rendering again

--- client-mobile/newOrg/Services/Routes.js
+++ client-mobile/newOrg/Services/Routes.js
@@ -1,32 +1,5 @@
-// var store = require('./Store');
-// var Actions = require('../Actions/Actions');
-
-function hometab() {
-
-  // console.log('rendering a hometab scene');
-
-  // var state = store.getState();
-
-  // var props = {
-  //   selectedHomeTab: state.uiMode.selectedHomeTab,
-    // gotoItemsTab() {
-    //   store.dispatch(Actions.setHomeTab('items'));
-    // },
-    // gotoReckoningsTab() {
-    //   store.dispatch(Actions.setHomeTab('reckonings'));
-    // },
-    // gotoSettingsTab() {
-    //   store.dispatch(Actions.setHomeTab('settings'));
-    // },
-  // };
-
-  return {
+exports.hometab = {
     name: 'hometab',
     component: require('../Components/Home/dumb/HomeTab'),
     title: 'Home',
   };
-}
-
-module.exports = {
-  hometab,
-};

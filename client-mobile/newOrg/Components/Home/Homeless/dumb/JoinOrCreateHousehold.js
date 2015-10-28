fbf57377507875505747cd9edbee32ff2031b6d3
amy-chiu <amy.lee.chiu@gmail.com>
2015-10-27 19:16:46 -0700

    fix jsx to render invite roommates page

--- client-mobile/newOrg/Components/Home/Homeless/dumb/JoinOrCreateHousehold.js
+++ client-mobile/newOrg/Components/Home/Homeless/dumb/JoinOrCreateHousehold.js
@@ -13,7 +13,7 @@ var {
 var JoinOrCreateHousehold = React.createClass({
   getInitialState() {
     return ({
-      invitations: this.props.invitations,
+      invitations: this.props.invitations.received,
       householdName: '',
       acceptedInvitation: '',
       declinedInvitation: '',
@@ -22,40 +22,67 @@ var JoinOrCreateHousehold = React.createClass({
 
   render() {
     var invitations = this.state.invitations;
-    return (
-      <View>
-        {
-          invitations.map(function(invitation) {
-            return (
-              <Text>You have been invited to the {invitation.householdName} household!</Text>
-              // <TouchableHighlight onPress={}>
-              //   <Text>Join</Text>
-              // </TouchableHighlight>
-              // <TouchableHighlight onPress={}>
-              //   <Text>Decline</Text>
-              // </TouchableHighlight>
-            );
-          })
-        }
-        <Text> ----- OR----- </Text>
-        <TextInput
-          style={styles.input}
-          keyboardType='default'
-          placeholder='What is your household name?'
-          onChangeText={(householdName) => this.setState({householdName: householdName})}
-          value={this.state.householdName}
-        />
-        <TouchableHighlight
-          style={styles.button}
-          onPress={() => {
-            this.handleSubmit();
-            this.props.gotoInviteRoommates();
-          }}
-        >
-          <Text style={styles.btnText}>Create household</Text>
-        </TouchableHighlight>
-      </View>
-    );
+    if (invitations.length === 0) {
+      return (
+        <View>
+          <Text>If your roommate has not invited you to the household yet, remind them. (FIX MESSAGING)</Text>
+          <Text> ----- OR ----- </Text>
+          <Text> Create a household </Text>
+          <TextInput
+            style={styles.input}
+            keyboardType='default'
+            placeholder='What is your household name?'
+            onChangeText={(householdName) => this.setState({householdName: householdName})}
+            value={this.state.householdName}
+          />
+          <TouchableHighlight
+            style={styles.button}
+            onPress={() => {
+              this.handleSubmit();
+              this.props.gotoInviteRoommates();
+            }}
+          >
+            <Text style={styles.btnText}>Create household</Text>
+          </TouchableHighlight>
+        </View>
+      );
+    } else {
+      return (
+        <View>
+          {
+            invitations.map(function(invitation) {
+              return (
+                <Text key={invitation.id}>You have been invited to the {invitation.householdName} household!</Text>
+                // <TouchableHighlight onPress={}>
+                //   <Text>Join</Text>
+                // </TouchableHighlight>
+                // <TouchableHighlight onPress={}>
+                //   <Text>Decline</Text>
+                // </TouchableHighlight>
+              );
+            })
+          }
+          <Text>----- OR-----</Text>
+          <Text>Create a household</Text>
+          <TextInput
+            style={styles.input}
+            keyboardType='default'
+            placeholder='What is your household name?'
+            onChangeText={(householdName) => this.setState({householdName: householdName})}
+            value={this.state.householdName}
+          />
+          <TouchableHighlight
+            style={styles.button}
+            onPress={() => {
+              this.handleSubmit();
+              this.props.gotoInviteRoommates();
+            }}
+          >
+            <Text style={styles.btnText}>Create household</Text>
+          </TouchableHighlight>
+        </View>
+      );
+    }
   },
 
   joinHousehold(invitation) {

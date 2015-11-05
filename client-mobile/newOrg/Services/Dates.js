e771950b9a4c008b09b4d148f61909720fe803c4
Kyle Cho <kylecho.work@gmail.com>
2015-11-04 21:32:43 -0800

    Fix month to display correctly

--- client-mobile/newOrg/Services/Dates.js
+++ client-mobile/newOrg/Services/Dates.js
@@ -1,16 +1,16 @@
 var months = {
-  '00': 'January',
-  '01': 'February',
-  '02': 'March',
-  '03': 'April',
-  '04': 'May',
-  '05': 'June',
-  '06': 'July',
-  '07': 'August',
-  '08': 'September',
-  '09': 'October',
-  '10': 'November',
-  '11': 'December',
+  '00': 'December',
+  '01': 'January',
+  '02': 'February',
+  '03': 'March',
+  '04': 'April',
+  '05': 'May',
+  '06': 'June',
+  '07': 'July',
+  '08': 'August',
+  '09': 'September',
+  '10': 'October',
+  '11': 'November',
 };
 
 var convertDate = function(date) {

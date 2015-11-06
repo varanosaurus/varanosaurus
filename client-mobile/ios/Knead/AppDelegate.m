f55128faadb1381b17c933bdb8403539d0190b14
Kyle Cho <kylecho.work@gmail.com>
2015-11-05 19:34:56 -0800

    Change mobile dev env to local env

--- client-mobile/ios/Knead/AppDelegate.m
+++ client-mobile/ios/Knead/AppDelegate.m
@@ -49,8 +49,8 @@
    * on the same Wi-Fi network.
    */
 
-  jsCodeLocation = [NSURL URLWithString:@"http://10.8.3.1:8081/index.ios.bundle?platform=ios&dev=true"]; // For Mobile
-  // jsCodeLocation = [NSURL URLWithString:@"http://localhost:8081/index.ios.bundle?platform=ios&dev=true"];
+  // jsCodeLocation = [NSURL URLWithString:@"http://10.8.3.1:8081/index.ios.bundle?platform=ios&dev=true"]; // For Mobile
+  jsCodeLocation = [NSURL URLWithString:@"http://localhost:8081/index.ios.bundle?platform=ios&dev=true"];
 
   /**
    * OPTION 2
